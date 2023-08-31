5fa0227007d6934ed94193c6bf0569203a652a54
joapedro12345 <joaopedromestreff2007@gmail.com>
2023-08-31 15:14:57 -0300

    setup do express

---
+++ 2_EXPRESS/2_setup/index.js
@@ -0,0 +1,8 @@
+// importar express
+const express = require('express')
+const app = express()
+
+// GET e POST
+app.get('/', (requisicao, resposta) => {
+    resposta.send('Estou utilizando o Express!')
+})
